6f0c743184ade10b7af27df686b417db825c859f
Dominique Anthony <124796974+Dom-Ant@users.noreply.github.com>
2023-05-31 00:04:44 -0400

    update

--- day01nodejs-large-api/nodejs-express-mysql/app/config/db.config.js
+++ day01nodejs-large-api/nodejs-express-mysql/app/config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    HOST: "sql.freedb.tech",
-    USER: "freedb_User01",
-    PASSWORD: "SJPT9c&s$Ryk5q$",
-    DB: "freedb_FreeDB_Bezkoder-Large-DA"
+    HOST: "localhost",
+    USER: "day02tutorial",
+    PASSWORD: "m09^aa63zRXR",
+    DB: "day02tutorial"
   };
